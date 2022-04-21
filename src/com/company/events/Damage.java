dcd2dcbb9cdb3d01e7fc3eeb9d553268ba47afe5
Ajhosky <dsobolewski@tm1.edu.pl>
2022-04-21 09:28:03 +0200

    JAKIES TAM ZMIANY DB DALEJ NIESKONCZONA

--- src/com/company/events/Damage.java
+++ src/com/company/events/Damage.java
@@ -1,5 +1,11 @@
 package com.company.events;
 
+import com.company.Offer.Policy;
+
 public class Damage {
     private DamageType damgaeType;
+    private int id;
+    private Policy policy;
+    private String documents;
+    private String victim; // String najprawdopodobniej zamienic na customers
 }
