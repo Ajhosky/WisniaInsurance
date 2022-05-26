88b6108ebb6501085bd1ed4cb78836f0a2dca9ca
Ajhosky <domin20050601@gmail.com>
2022-05-26 09:11:01 +0200

    .

--- src/com/company/events/Damage.java
+++ src/com/company/events/Damage.java
@@ -8,4 +8,44 @@ public class Damage {
     private Policy policy;
     private String documents;
     private String victim; // String najprawdopodobniej zamienic na customers
+
+    public DamageType getDamgaeType() {
+        return damgaeType;
+    }
+
+    public void setDamgaeType(DamageType damgaeType) {
+        this.damgaeType = damgaeType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Policy getPolicy() {
+        return policy;
+    }
+
+    public void setPolicy(Policy policy) {
+        this.policy = policy;
+    }
+
+    public String getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(String documents) {
+        this.documents = documents;
+    }
+
+    public String getVictim() {
+        return victim;
+    }
+
+    public void setVictim(String victim) {
+        this.victim = victim;
+    }
 }
