081edc6343b1038fce1226d87e37d2a8750c2633
Uczeń <domin20050601>
2022-06-09 09:26:36 +0200

    dodanie przyciskow i napisu o bledzie

--- src/com/company/menuPanel/MenuPanel.java
+++ src/com/company/menuPanel/MenuPanel.java
@@ -7,8 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MenuPanel implements Mediator, ActionListener {
-    JButton button;
-    JFrame frame;
+    JFrame window;
+    JPanel left;
 private PolicyListButton policyListButton;
 private List list;
 
@@ -34,43 +34,58 @@ private List list;
     @Override
     public void createGUI() {
         JFrame window = new JFrame("Wisnia Insurance menu");
-
         window.setSize(960,600);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setLayout(null);
-        window.setResizable(false);
-        window.setLocationRelativeTo(null);
         window.setVisible(true);
         JPanel left = new JPanel();
+        left.setBounds(0,0,100,100);
+        left.setBackground(Color.gray);
         left.setBorder(new LineBorder(Color.BLACK));
-        left.setSize(320, 600);
-        left.setLayout(new BoxLayout(left, BoxLayout.Y_AXIS));
-        JPanel buttonPanel = new JPanel();
-        policyListButton.setPreferredSize(new Dimension(85, 25));
-        buttonPanel.add(policyListButton);
-        buttonPanel.setLayout(new FlowLayout());
-        JPanel listPanel = new JPanel();
-       // list.setFixedCellWidth(260);
-        listPanel.setSize(320, 470);
-        left.add(buttonPanel);
-        left.add(listPanel);
+        JButton b1=new JButton("Sign in");
+        b1.setBounds(50,100,80,30);
+        b1.setBackground(Color.yellow);
+        JButton b2=new JButton("Sign up");
+        b2.setBounds(100,100,80,30);
+        b2.setBackground(Color.green);
+        left.add(b1);
+        left.add(b2);
+        window.add(left);
+        Font font = new Font("Courier", Font.BOLD,25);
+        JLabel error = new JLabel("Something went wrong");
+        error.setText("Something went wrong");
+        error.setVisible(false);
+        error.setFont(font);
+        left.add(error);
+        ActionListener SignInButton = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                error.setVisible(true);
+            }
+        };
+        b1.addActionListener(SignInButton);
+        b2.addActionListener(SignInButton);
 
     }
-    public void JButtonTest(){
-        frame = new JFrame();
-        frame.setTitle("My JFrame");
-        frame.setSize(500, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        button = new JButton("Click Me!");
-        button.addActionListener(this);
 
-        frame.setLayout(new FlowLayout());
-        frame.add(button);
-        frame.setVisible(true);
-    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("hello");
+
     }
+
+    //    public void JButtonTest(){
+//        frame = new JFrame();
+//        frame.setTitle("My JFrame");
+//        frame.setSize(500, 400);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        button = new JButton("Click Me!");
+//        button.addActionListener(this);
+//
+//        frame.setLayout(new FlowLayout());
+//        frame.add(button);
+//        frame.setVisible(true);
+//    }
+
 }
